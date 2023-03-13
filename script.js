6f56dc5004535fbbdc6152c3ffd9a08ba86ada6d
Praveenive <124970010+Praveenive@users.noreply.github.com>
2023-03-13 12:21:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function getelement(){
+    let res=document.getElementById("firstname").value;
+    console.log(res);
+    let res2=document.getElementById("Middlename").value;
+    console.log(res2);
+    let res1=document.getElementById("Lastname").value;
+    console.log(res1);
+    let res3=document.getElementById("email").value;
+    console.log(res3);
+    let res4=document.getElementById("pass").value;
+    console.log(res4);
+}
